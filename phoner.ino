3c4a989e25f01c2c8a471a3998ed651a31a6951b
p100032 <f.senny@helmo.be>
2024-12-12 22:36:36 +0100

    Refresh code with sendAT function

--- phoner.ino
+++ phoner.ino
@@ -21,12 +21,15 @@ HardwareSerial sim800l(1); // RX, TX
 const int RX_PIN = 4;
 const int TX_PIN = 5;
 
-#define CALL_FRED 1
+// #define CALL_FRED
 #define SLEEP_MODE 2 // 1 (using DTR pin) or 2
 
 // These pins are connected on the PCB, but not used for the moment => define as inputs
 const int RX_PIN_UNUSED_ON_BOARD = 16;
 const int TX_PIN_UNUSED_ON_BOARD = 17;
+
+#define TIMEOUT_SIM 3000
+#define DELAY_SLEEP_SIM 5000
 
 /// Button
 #define BTN_GPIO GPIO_NUM_6 // PIN 6/D12 = LP GPIO for wake up ESP32 /!\ used for RED LED on PCB
@@ -36,7 +39,6 @@ const int TX_PIN_UNUSED_ON_BOARD = 17;
 
 /// DTR pin - external wire
 #define DTR_GPIO GPIO_NUM_19
-
 
 /// Battery
 const int VBAT_PIN = 0;
@@ -48,14 +50,16 @@ const int led = 15;
 /// Setup logic
 RTC_DATA_ATTR int bootCount = 0;
 
+// need function prototype
+bool sendATCommand(HardwareSerial &serial, const char *message,const char *command, const char *expectedResponse, unsigned long timeout);
+
 void setup()
 { 
   /*
    * INITIAL SETUP
-   *
   */
 
-  blink(500);
+  blink(500,1);
   /// On-board led
   pinMode(led, OUTPUT);
   pinMode(RST_GPIO,OUTPUT);
@@ -69,78 +73,59 @@ void setup()
   pinMode(BTN_GPIO, INPUT);          // I made it as a pullup button, so use PULLDOWN internal resistor with rtc functions (see after call deep_sleep)
                                      // change input arg of deep sleep function esp_sleep_enable_ext1_wakeup accordingly
                                      // it seems that a pullup button is better, the wake up condition on HIGH input is better
+  
+  /// Battery check
+  analogReadResolution(12);
 
+  // setup SIM800L pin digital level
   digitalWrite(DTR_GPIO,LOW);
   digitalWrite(RST_GPIO,HIGH);
   delay(100);
-
-  /// Battery check
-  analogReadResolution(12);
 
   // Setup Serial communications
   Serial.begin(9600); // Serial monitor comm
   Serial.write("Serial initialized\n");
 
-  Serial.print("Bootcount = ");
-  Serial.println(bootCount);
-  for(int i=0; i<bootCount; i++)
-  {
-    blink(1000);
-  }
-  //bootCount++; // do it later
-
   // Setup sim800l module
   Serial.write("Initializing SIM800L ...");
   sim800l.begin(9600, SERIAL_8N1, RX_PIN, TX_PIN); // Works with HardwareSerial(1)
   Serial.write("done\n");
+  delay(1000); // need a delay here
+}
+
+void loop()
+{
+  // Bootcount counts the number of boot :D
+  Serial.print("Bootcount = ");
+  Serial.println(bootCount);
+  blink(1000,bootCount);
   
   if (bootCount == 0)
   {
-    // hard reset module SIM800L with pin RST
-    //resetSIM800L();
-    Serial.write("Starting handshake...\n");
-    sim800l.println("AT"); //Autobaud, Once the handshake test is successful, it will back to OK
-    updateSerial();
-    delay(2000); // why 10 seconds ?
-
-    Serial.write("Signal quality test...\n");
-    sim800l.println("AT+CSQ"); //Signal quality test, value range is 0-31 , 31 is the best
-    updateSerial();
-
-    Serial.write("Reading SIM information...\n");
-    sim800l.println("AT+CCID"); //Read SIM information to confirm whether the SIM is plugged
-    updateSerial();
-
-    Serial.write("Selecting mobile operator...\n");
-    sim800l.println("AT+COPS=0,2"); // select the mobile network operator| AT+COPS=<mode>(0 for auto),<format>(2 for numeric),<oper>,<AcT>
-    updateSerial();
-
-    Serial.write("Checking network status...\n");
-    sim800l.println("AT+CREG?"); //check the network registration status. Will answer +CREG: <n>,<stat>
-    updateSerial();
-
-    Serial.write("Querying battery status...\n");
-    sim800l.println("AT+CBC"); // Querry battery status
-    updateSerial();
-
-    // Blink LED twice at the end of boot sequence
-    blink(200);
-    blink(200);
-    blink(200);
+    start_sim800L();
+    // Blink LED at the end of boot sequence
+    blink(200,3);
   }
 
   bootCount++;
-
   if(bootCount > 0 )
   {
     wake_up_sim800L();
-    Serial.println("Who do you call ?");
     call();
   }
 
-  Serial.println("");
-  Serial.println("Going to sleep now");
   sleep_sim800L();
+  sleep_esp32();
+  Serial.println("This will never be printed");
+}
+
+// ------------------------------------------------------------- //
+// ESP32 functions
+// ------------------------------------------------------------- //
+// sleep_esp32()
+
+void sleep_esp32()
+{
   // TODO: use ANY_LOW if pull-up
   // https://randomnerdtutorials.com/esp32-deep-sleep-arduino-ide-wake-up-sources/
   esp_sleep_enable_ext1_wakeup((1ULL << BTN_GPIO), ESP_EXT1_WAKEUP_ANY_HIGH);
@@ -148,18 +133,55 @@ void setup()
                                                 // idt does not work ?? https://github.com/espressif/esp-idf/issues/11932
   rtc_gpio_pulldown_en(BTN_GPIO);  // GPIO6 is tie to GND in order to wake up in HIGH
   rtc_gpio_pullup_dis(BTN_GPIO);   // Disable PULL_UP in order to allow it to wakeup on HIGH
-
   // reminder : COM port is disabled, so board does show disconnected in IDE
   esp_deep_sleep_start();
-  Serial.println("This will never be printed");
 }
 
-void loop()
+// ------------------------------------------------------------- //
+// SIM800L functions
+// ------------------------------------------------------------- //
+// sendATCommand()
+// start_sim800L()
+// reset_sim800L()
+// sleep_sim800L()
+// wake_up_sim800L()
+// call()
+
+// Send and check AT Commeand
+// use : sendATCommand(sim800l,"Message for serial Monitor","AT","OK",DELAY_SLEEP_SIM)
+bool sendATCommand(HardwareSerial &serial, const char *message,const char *command, const char *expectedResponse, unsigned long timeout = DELAY_SLEEP_SIM) {
+    serial.println(command);
+    unsigned long startTime = millis();
+    String response = "";
+
+    while (millis() - startTime < timeout) {
+        while (serial.available()) {
+            char c = serial.read();
+            response += c;
+            // Vérifiez si la réponse attendue est contenue
+            if (response.indexOf(expectedResponse) != -1) {
+                Serial.println(String(message) + " Command " + String(command) + "->\n" + response);
+                return true;
+            }
+        }
+        delay(1);
+    }
+    Serial.println(String(message) + " Command " + String(command) + " failed:\n" + response);
+    return false;
+}
+
+void start_sim800L()
 {
-  // Never called
+  sendATCommand(sim800l,"Starting handshake...","AT","OK",TIMEOUT_SIM); // Starting handshake
+  sendATCommand(sim800l,"Signal quality test...","AT+CSQ","OK",TIMEOUT_SIM); //Signal quality test, value range is 0-31 , 31 is the best
+  sendATCommand(sim800l,"Reading SIM information...","AT+CCID","OK",TIMEOUT_SIM); //Read SIM information to confirm whether the SIM is plugged
+  sendATCommand(sim800l,"Selecting mobile operator...","AT+COPS=0,2","OK",TIMEOUT_SIM); // select the mobile network operator| AT+COPS=<mode>(0 for auto),<format>(2 for numeric),<oper>,<AcT>
+  // there is an error here
+  sendATCommand(sim800l,"Checking network status...","AT+CREG?","OK",TIMEOUT_SIM); //check the network registration status. Will answer +CREG: <n>,<stat>
+  sendATCommand(sim800l,"Querying battery status...","AT+CBC","OK",TIMEOUT_SIM); // Querry battery status
+    
 }
-
-void resetSIM800L()
+void reset_sim800L()
 {
   digitalWrite(RST_GPIO,HIGH);
   delay(200);
@@ -172,28 +194,21 @@ void resetSIM800L()
 // RF OFF : datasheet page 26, section 4.3
 void sleep_sim800L()
 {
-  Serial.println("Set RF off");
-  delay(100); // to check, sometimes, I do not have a feedback about the next AT command which is crucial for RF off
-  sim800l.println("AT+CFUN=4"); // RF off with reset MT
-  updateSerial(); // OK is expected
-  delay(5000); // should reply within 10 sec max according to AT CFUN page 96
+  Serial.println("");
+  Serial.println("Going to sleep now");
+  sendATCommand(sim800l,"Set RF off","AT+CFUN=4","OK",TIMEOUT_SIM);
+  delay(DELAY_SLEEP_SIM); // should reply within 10 sec max according to AT CFUN page 96
   if (SLEEP_MODE == 1)
   {
-    Serial.println("\nSet sleep mode 1");
-    sim800l.println("AT+CSCLK=1"); // sleep mode 1, actually, need just once
-                                   // afterwards, just use the DTR pin
-    updateSerial(); // OK is expected
+    sendATCommand(sim800l,"\nSet sleep mode 1","AT+CSCLK=1","OK",DELAY_SLEEP_SIM);
     delay(100);
     digitalWrite(DTR_GPIO,HIGH); // put high for sleep mode
     delay(100);
   }
   else if (SLEEP_MODE == 2)
   {
-    Serial.println("\nSet sleep mode 2");
-    sim800l.println("AT+CSCLK=2"); // sleep mode 2
-    updateSerial(); // OK is expected
-    delay(5000); // wait for at least 5s without UART, on air or IO INTR
-    // 
+    sendATCommand(sim800l,"\nSet sleep mode 2","AT+CSCLK=2","OK",DELAY_SLEEP_SIM);
+    delay(DELAY_SLEEP_SIM); // wait for at least 5s without UART, on air or IO INTR
   }
   else 
   {
@@ -205,7 +220,7 @@ void sleep_sim800L()
 void wake_up_sim800L()
 {
   Serial.println("Wake up SIM800L");
-  sim800l.println("AT"); // dummy data
+  sim800l.println("AT"); // dummy data, wait no response, so do not use sendATCommand
   delay(200); // datasheet AT command page 153, note 2 : apply a delay of (at least) 100ms between dummy/waking data and AT command
   Serial.println("\nDisable sleep mode");
   if (SLEEP_MODE == 1)
@@ -215,73 +230,19 @@ void wake_up_sim800L()
   }
   else if (SLEEP_MODE == 2)
   {
-    sim800l.println("AT+CSCLK=0"); // go out from sleep mode 2
-    updateSerial(); // OK is expected
-    delay(5000); // wait for at least 5s without UART, on air or IO INTR
-    // 
+    sendATCommand(sim800l,"","AT+CSCLK=0","OK",DELAY_SLEEP_SIM);
+    delay(DELAY_SLEEP_SIM); // wait for at least 5s without UART, on air or IO INTR
   }
   else 
   {
     // no SLEEP mode
   }
-  
-  sim800l.println("AT+CFUN=1"); // normal function, wake up RF
-  updateSerial(); // OK is expected
-  // May check using AT+CFUN? and AT+CSCLK?
-  delay(5000); // should reply within 10 sec max according to AT CFUN page 96
-}
-
-void blink(const int millis)
-{
-  digitalWrite(led, LOW);
-  delay(millis);
-  digitalWrite(led, HIGH);
-  delay(millis);
-  digitalWrite(led, LOW);
-}
-
-void blinkForBattery()
-{
-  // ADC is 12 bits precision -> 0 to 4095
-  int rawValue = analogRead(VBAT_PIN); // Voltage from ADC [0;4095]
-  // Vref is 3.3V so 4095 correspond to 3.3V.
-  float voltage = (rawValue * VREF) / 4095.0; // Convert to actual volts (max 3.3V)
-
-  // The actual max battery voltage is around 4.1V
-  // The critical battery voltage is below 3.5V
-  // The mesurement is actually VBAT/2 => MAX = 2.05 and min = 1.75
-
-  int voltage_deca = voltage * 10/ 3.3; // Scale to [0-10] to blink up to 10 times, representing the current voltage
-
-  // Now blink up to 10 times, showing more precision
-  // It blinks 7 times when full. In theory, full = 4.2V -> 2.1V measured -> 0.63/10 -> blink 6 times.
-  // It blinks TBC times when on battery for a lil while. In theory 3.7V -> 1.85V measured -> 0.56/10 -> blink 5 times.
-  int voltage_deca_precise = (voltage-1) * 10;
-  for(int i=0; i < voltage_deca_precise; ++i)
-  {
-    blink(500);
-  }
-
-  Serial.print("Battery Voltage: ");
-  Serial.print(voltage);
-  Serial.println(" V");
-}
-
-void updateSerial()
-{
-  delay(500); // wait for any incoming data
-  while (Serial.available()) 
-  {
-    sim800l.write(Serial.read());//Forward what Serial received to Software Serial Port
-  }
-  while(sim800l.available()) 
-  {
-    Serial.write(sim800l.read());//Forward what Software Serial received to Serial Port
-  }
+  sendATCommand(sim800l,"","AT+CFUN=1","OK",DELAY_SLEEP_SIM);
 }
 
 void call()
 {
+  Serial.println("Who do you call ?");
   // CALL
   Serial.write("Calling...\n");
 #ifdef CALL_FRED
@@ -303,3 +264,63 @@ void call()
   sim800l.println("ATH"); //hang up
   updateSerial();
 }
+
+// ------------------------------------------------------------- //
+// Misc functions
+// ------------------------------------------------------------- //
+
+void updateSerial()
+{
+  delay(500); // wait for any incoming data
+  while (Serial.available()) 
+  {
+    sim800l.write(Serial.read());//Forward what Serial received to Software Serial Port
+  }
+  while(sim800l.available()) 
+  {
+    Serial.write(sim800l.read());//Forward what Software Serial received to Serial Port
+  }
+}
+
+// blink N times the LED with a period of 2*millis
+void blink(const int millis,int N)
+{
+  for (int i=0;i<N;i++)
+  {
+    digitalWrite(led, LOW);
+    delay(millis);
+    digitalWrite(led, HIGH);
+    delay(millis);
+    digitalWrite(led, LOW);
+  }
+  
+}
+
+void blinkForBattery()
+{
+  // ADC is 12 bits precision -> 0 to 4095
+  int rawValue = analogRead(VBAT_PIN); // Voltage from ADC [0;4095]
+  // Vref is 3.3V so 4095 correspond to 3.3V.
+  float voltage = (rawValue * VREF) / 4095.0; // Convert to actual volts (max 3.3V)
+
+  // The actual max battery voltage is around 4.1V
+  // The critical battery voltage is below 3.5V
+  // The mesurement is actually VBAT/2 => MAX = 2.05 and min = 1.75
+
+  int voltage_deca = voltage * 10/ 3.3; // Scale to [0-10] to blink up to 10 times, representing the current voltage
+
+  // Now blink up to 10 times, showing more precision
+  // It blinks 7 times when full. In theory, full = 4.2V -> 2.1V measured -> 0.63/10 -> blink 6 times.
+  // It blinks TBC times when on battery for a lil while. In theory 3.7V -> 1.85V measured -> 0.56/10 -> blink 5 times.
+  int voltage_deca_precise = (voltage-1) * 10;
+  for(int i=0; i < voltage_deca_precise; ++i)
+  {
+    blink(500,1);
+  }
+  // blink(500,voltage_deca_precise);
+
+  Serial.print("Battery Voltage: ");
+  Serial.print(voltage);
+  Serial.println(" V");
+}
+
